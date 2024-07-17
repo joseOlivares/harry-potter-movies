d4a3a0562e5d72ab9a3ec90e85c39ab3297ea37b
José Luis Olivares <joseluis_503@yahoo.com>
2024-07-17 16:44:53 -0600

    [ADD] creating filter-view component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FilterViewComponent } from './components/filter-view/filter-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: FilterViewComponent }
+];

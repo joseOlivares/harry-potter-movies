aad99be88923854a04a3d30fe54979dd6a160297
José Luis Olivares <joseluis_503@yahoo.com>
2024-07-19 19:03:06 -0600

    [FIX] setting angular environment fpr Vercel

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, Signal, signal } from '@angular/core';
 import { Movie, MovieDetails, MovieId } from '../types/types';
-import { API_URL } from '../constants/constants';
+//import { API_URL } from '../constants/constants';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
@@ -15,18 +15,18 @@ export class MovieService {
 
   private http=inject(HttpClient);
 
- // private API_URL=environment.api_url;
+  private API_URL=environment.api_url;
 
   constructor() { }
 
   getAllMovies(): Observable<Movie[]> {
-   return this.http.get<Movie[]>( `${API_URL}/movies` );
+   return this.http.get<Movie[]>( `${this.API_URL}/movies` );
   }
 
   getMovieDetails(id: MovieId): Observable<MovieDetails> {
     //console.log("Service -- solicitando id= "+id)
 
-    return this.http.get<MovieDetails>( `${API_URL}/movies/${id}` );
+    return this.http.get<MovieDetails>( `${this.API_URL}/movies/${id}` );
   }
 
 }

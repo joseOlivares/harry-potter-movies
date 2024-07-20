eab94a674966e8089de8b744b354ae124c07a6c5
José Luis Olivares <joseluis_503@yahoo.com>
2024-07-19 18:52:22 -0600

    [FIX] Configuring to Vercel

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,2 +1,4 @@
-//local constants
-export const API_URL = "http://localhost:4200";
+//API constants
+//export const API_URL = "http://localhost:4200";
+
+export const API_URL = "https://harry-potter-movies-pink.vercel.app/";
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url : "https://harry-potter-movies-mocha.vercel.app",
+  api_url : "https://harry-potter-movies-pink.vercel.app",
 };

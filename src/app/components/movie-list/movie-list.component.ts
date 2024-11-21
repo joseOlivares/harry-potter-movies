c16df7f2d2cce4db9fd62437aa1d9ff015a731b7
José Luis Olivares <joseluis_503@yahoo.com>
2024-11-21 15:14:23 -0600

    [ADD] Author in main page

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -2,12 +2,12 @@ import { Component, input } from '@angular/core';
 import { Movie, MovieId } from '../../types/types';
 import { DurationPipe } from '../../pipes/duration.pipe';
 import { MillionPipe } from '../../pipes/million.pipe';
-import { Router, RouterLink } from '@angular/router';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [DurationPipe, MillionPipe, RouterLink],
+  imports: [DurationPipe, MillionPipe],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
